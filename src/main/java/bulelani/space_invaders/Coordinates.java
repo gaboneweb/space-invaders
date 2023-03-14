3a1127b92869416ff3f4b91aba5e3160ca48b0f9
bugabon022 <bugabon022@student.wethinkcode.co.za>
2023-03-14 11:20:02 +0200

    Added the Coordinates class

---
+++ src/main/java/bulelani/space_invaders/Coordinates.java
@@ -0,0 +1,31 @@
+package bulelani.space_invaders;
+
+public class Coordinates {
+    private float xPos;
+    private float yPos;
+
+    public Coordinates(float x , float y){
+        this.xPos = x;
+        this.yPos = y;
+    }
+
+    public Coordinates(){
+        this((float)0,(float) 0);
+    }
+
+    public void setXpos(float x){
+        this.xPos = x;
+    }
+
+    public void setYpos(float y){
+        this.yPos = y;
+    }
+
+    public float getXpos(){
+        return this.xPos;
+    }
+
+    public float getYpos(){
+        return this.yPos;
+    }
+}
